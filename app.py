6dbfebe2da1496b8bddff3152e7929dc4bc98791
darlim55 <jorgedarlim@gmail.com>
2022-11-16 16:31:18 -0400

    versao 1.4

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def create_app(test_config=None):
     
     @app.route('/movies', methods=['GET'])
     @requires_auth('view:movies')
-    def retrieve_movies():
+    def retrieve_movies(payload):
         movies = Movie.query.all()
         return jsonify({
             "success": True,
@@ -46,7 +46,7 @@ def create_app(test_config=None):
 
     @app.route('/actors', methods=['GET'])
     @requires_auth('get:actors')
-    def retrieve_actors():
+    def retrieve_actors(payload):
         actors = Actor.query.all()
         return jsonify({
             "success": True,
